70ff43f29caa00f502d7ad6e18cfd128526b1a7a
Melvin Jones Repol <mrepol742@gmail.com>
2026-02-07 11:04:50 +0800

    feat: simplify laravel log output

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,18 +6,31 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(
-            except: ['/api/*']
-        );
+        $middleware->validateCsrfTokens(except: ['/api/*']);
         $middleware->alias([
             'verify.session' => \App\Http\Middleware\VerifySession::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        // This simplify laravel exception logging by only logging relevant frames
+        $exceptions->report(function (Throwable $e) {
+            $trace = collect($e->getTrace())
+                ->filter(
+                    fn($frame) => isset($frame['file']) &&
+                        str_contains($frame['file'], base_path('app')),
+                )
+                ->take(5)
+                ->values()
+                ->all();
+
+            logger()->error($e->getMessage(), ['trace' => $trace]);
+
+            return false;
+        });
+    })
+    ->create();
